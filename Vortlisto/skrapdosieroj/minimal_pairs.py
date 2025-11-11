d5a0499e35a3024668182ea35efdae975a59dfab
gantar22 <nathancarterwilliams@gmail.com>
2025-11-11 12:18:50 -0500

    update

--- Vortlisto/skrapdosieroj/minimal_pairs.py
+++ Vortlisto/skrapdosieroj/minimal_pairs.py
@@ -1,18 +1,18 @@
 import sys
 from collections import defaultdict
 
-def hat_pairs(words : dict[str,int]) -> dict[str,int]:
+def hat_pairs(words : set[str]) -> dict[str,int]:
     def unhat(c : str) -> str:
         unhat_map : dict[str,str] = {'ĉ':'c','ŝ':'s','ĝ':'g','ŭ':'u','ĵ':'j','ĥ':'h'}
-        unhatted = unhat_map.get(c)
-        if unhatted != None:
-            return unhatted
-        return c
+        other_map : dict[str,str] = {'c':'s','t':'d'}
+        unhatted = unhat_map.get(c,c)
+        unhatted = other_map.get(unhatted,unhatted)
+        return unhatted
 
     pairs : dict[str,list[tuple[str,int]]] = defaultdict(list) # Map from hatless string to all matching valid strings (the hatless one might not be a real word)
-    for (w,s) in words.items():
+    for w in words:
         key = ''.join([unhat(c) for c in w])
-        pairs[key].append((w,s))
+        pairs[key].append((w,50))
         
     result : dict[str,int] = {}
     for scores in [scores for (k,scores) in pairs.items() if len(scores) > 1]:
@@ -27,7 +27,7 @@ def get_words(paths : list[str]) -> dict[str,int]:
     for p in paths:
         with open(p,'r') as fd:
             for line in fd.readlines():
-                for word in line.split():
+                for word in [ww for w in line.split() for ww in w.split(';')]:
                     words[word] = 50
     return words
 
@@ -36,9 +36,21 @@ def write_words(out_file : str, scores : dict[str,int]):
         for (w,s) in scores.items():
             fd.write(f'{w};{s}\n')
 
-files = ['roots.txt']#['bitlibroj','sheetsdoc','tekstaro','vortlisto']
-files = [f'./Vortlisto/artefaktoj/{f}' for f in files]
-base_words = get_words(files)
-pairs = hat_pairs(base_words)
+roots = ['roots.txt']#['bitlibroj','sheetsdoc','tekstaro','vortlisto']
+roots = [f'./Vortlisto/artefaktoj/{f}' for f in roots]
+base_words = get_words(roots)
+
+words = ['bitlibroj','permane','tekstaro','vortlisto']
+words = [f'./Vortlisto/artefaktoj/{f}.dict' for f in words]
+full_words = get_words(words)
+
+words_to_use = set()
+for (w,s) in full_words.items():
+    if w[:-1] in base_words.keys():
+        if w[-1] in ['o','i','e','a']:
+            words_to_use.add(w)
+
+
+pairs = hat_pairs(words_to_use)
 pairs = {w:s + 100 for (w,s) in pairs.items()}
 write_words('./Vortlisto/artefaktoj/hat_pairs.dict',pairs)
